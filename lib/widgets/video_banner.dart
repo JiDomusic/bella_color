26009617b14aa73f72e80cf258f6a9888c6f2039
JiDomusic <jidomusic@gmail.com>
2026-03-28 21:05:05 -0300

    stocke arreglo y  bannner video html

--- lib/widgets/video_banner.dart
+++ lib/widgets/video_banner.dart
@@ -1,5 +1,6 @@
+import 'dart:html' as html;
+import 'dart:ui_web' as ui_web;
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 
 class VideoBanner extends StatefulWidget {
   final String videoUrl;
@@ -11,67 +12,62 @@ class VideoBanner extends StatefulWidget {
   State<VideoBanner> createState() => _VideoBannerState();
 }
 
-class _VideoBannerState extends State<VideoBanner> {
-  late VideoPlayerController _controller;
-  bool _initialized = false;
-  bool _error = false;
-  String _errorMsg = '';
+class _VideoBannerState extends State<VideoBanner> with SingleTickerProviderStateMixin {
+  late String _viewId;
+  bool _visible = true;
+  double _opacity = 1.0;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl))
-      ..setLooping(true)
-      ..setVolume(0)
-      ..initialize().then((_) {
-        if (mounted) {
-          setState(() => _initialized = true);
-          _controller.play();
-        }
-      }).catchError((e) {
-        debugPrint('VideoBanner error: $e');
-        if (mounted) setState(() { _error = true; _errorMsg = e.toString(); });
-      });
-  }
+    _viewId = 'video-banner-${widget.videoUrl.hashCode}';
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    final video = html.VideoElement()
+      ..src = widget.videoUrl
+      ..autoplay = true
+      ..muted = true
+      ..loop = false
+      ..style.width = '100%'
+      ..style.height = '100%'
+      ..style.objectFit = 'cover'
+      ..style.borderRadius = '14px'
+      ..setAttribute('playsinline', 'true');
+
+    ui_web.platformViewRegistry.registerViewFactory(_viewId, (int viewId) => video);
+
+    // A los 18s empieza fade out, a los 20s desaparece
+    Future.delayed(const Duration(seconds: 18), () {
+      if (mounted) setState(() => _opacity = 0.0);
+    });
+    Future.delayed(const Duration(seconds: 20), () {
+      if (mounted) setState(() => _visible = false);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_error) {
-      return Container(
+    if (!_visible) return const SizedBox.shrink();
+    return AnimatedOpacity(
+      opacity: _opacity,
+      duration: const Duration(seconds: 2),
+      child: Container(
         width: double.infinity,
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-        padding: const EdgeInsets.all(12),
+        height: 220,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         decoration: BoxDecoration(
-          color: Colors.red.withAlpha(40),
-          borderRadius: BorderRadius.circular(14),
-        ),
-        child: Text('Error al cargar video: $_errorMsg',
-          style: const TextStyle(color: Colors.redAccent, fontSize: 12)),
-      );
-    }
-    return Container(
-      width: double.infinity,
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(14),
-        border: Border.all(color: widget.borderColor.withAlpha(60)),
-      ),
-      clipBehavior: Clip.antiAlias,
-      child: _initialized
-          ? AspectRatio(
-              aspectRatio: _controller.value.aspectRatio,
-              child: VideoPlayer(_controller),
-            )
-          : const SizedBox(
-              height: 120,
-              child: Center(child: CircularProgressIndicator(strokeWidth: 2)),
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: widget.borderColor.withAlpha(80), width: 1.5),
+          boxShadow: [
+            BoxShadow(
+              color: widget.borderColor.withAlpha(40),
+              blurRadius: 12,
+              offset: const Offset(0, 4),
             ),
+          ],
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: HtmlElementView(viewType: _viewId),
+      ),
     );
   }
 }
